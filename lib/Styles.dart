63cde6504c11cd600d0d054b23d721c3a394f232
engineer-fahad <fahad.work20@gmail.com>
2022-02-04 19:57:43 +0300

    Fahad V1

---
+++ lib/Styles.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+//Colors
+const blue_base = Color(0x4799B7);
+const blue_smooth = Color(0xCFE3EA);
+
+const yellow_base = Color(0xF3A72E);
+const yellow_smooth = Color(0xFCF3D7);
+
+const green_base = Color(0x2FA84F);
+const green_smooth = Color(0xDCF7E3);
+
+const red_base = Color(0xEA3D2F);
+const red_smooth = Color(0xFEE4E2);
+
+// icon color
+const iconscolor = Color(0x78909C);
+//Fonts

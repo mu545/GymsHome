4364a1477c041e71adbb8966d01c46059a09cf10
mu545 <alomer241@gmail.com>
2022-03-12 17:23:04 +0300

    MH38

--- lib/widgets/resetpass.dart
+++ lib/widgets/resetpass.dart
@@ -42,7 +42,7 @@ class _resetpasswordState extends State<resetpassword> {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Text(
-                    "Reset password",
+                    "Reset assword",
                     style: TextStyle(
                         fontFamily: 'Epilogue',
                         fontSize: 18,
